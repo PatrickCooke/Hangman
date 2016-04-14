5f8b645989418aa7ce2d7b7e32aba5a3e2e09fb2
PatrickCooke <cookepa1@gmail.com>
2016-04-14 14:35:26 -0400

    core method, creates string from csv, NSLogs random word

--- hangman/ViewController.m
+++ hangman/ViewController.m
@@ -8,20 +8,52 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+
+@property(nonatomic,strong)     NSArray     *wordSetArray;
+@property(nonatomic,strong)     NSString    *currentWord;
 
 @end
 
 @implementation ViewController
 
+NSString *wordListString = @"";
+
+#pragma mark - Core Methods
+
+- (NSString *)readBundleFileToString:(NSString *)filename ofType:(NSString *)type {
+    NSString *path = [[NSBundle mainBundle] pathForResource:filename ofType:type];
+    return [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
+}
+
+- (NSArray *)convertCSVStringToArray:(NSString *)csvString {
+    NSString *cleanString = [[csvString componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]] componentsJoinedByString:@""];
+    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@","];
+    return [cleanString componentsSeparatedByCharactersInSet:set];
+}
+
+#pragma mark - Interactivity Methods
+
+
+-(IBAction)startNewGame:(id)sender {
+    int randomWordIndex = arc4random_uniform((u_int32_t)_wordSetArray.count);
+    _currentWord = _wordSetArray[randomWordIndex];
+    NSLog(@"%@", _currentWord);
+}
+
+
+#pragma mark - Life Cycle Method
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *wordString = [self readBundleFileToString:@"WordSetApple" ofType:@"csv"];
+    _wordSetArray = [self convertCSVStringToArray:wordString];
+    NSLog(@"Count %li",_wordSetArray.count);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
