d51bb47e9f21686d0602590a4a07735b53b05159
PatrickCooke <cookepa1@gmail.com>
2016-04-14 11:34:39 -0400

    Initial Commit

---
+++ hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hangman
+//
+//  Created by Patrick Cooke on 4/14/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hangman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hangman
+//
+//  Created by Patrick Cooke on 4/14/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
